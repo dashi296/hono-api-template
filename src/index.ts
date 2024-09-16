4ade9f83c0ccc42007bff2c949eec5a0b78559ff
shun okada <falcon0296@gmail.com>
2024-09-16 12:22:22 +0900

    [add] zod-openapi, swagger-ui

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,42 @@
-import { Hono } from "hono";
+import { swaggerUI } from "@hono/swagger-ui";
+import { OpenAPIHono, createRoute, z } from "@hono/zod-openapi";
 
-const app = new Hono();
+const app = new OpenAPIHono();
 
-app.get("/", (c) => {
-	return c.text("Hello Hono!");
+app.openapi(
+	createRoute({
+		method: "get",
+		path: "/hello",
+		responses: {
+			200: {
+				description: "Respond a message",
+				content: {
+					"application/json": {
+						schema: z.object({
+							message: z.string(),
+						}),
+					},
+				},
+			},
+		},
+	}),
+	(c) => {
+		return c.json({
+			message: "hello",
+			hoge: "hoge",
+		});
+	},
+);
+
+// Use the middleware to serve Swagger UI at /ui
+app.get("/ui", swaggerUI({ url: "/doc" }));
+
+app.doc("/doc", {
+	info: {
+		title: "Hono Template API",
+		version: "v1",
+	},
+	openapi: "3.1.0",
 });
 
 export default app;
